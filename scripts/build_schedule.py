46c19c0299cbedabe439d32d80b667b7e8b804fb
marcomilanesio <milanesio.marco@gmail.com>
2019-09-28 00:44:17 +0200

    day date

--- scripts/build_schedule.py
+++ scripts/build_schedule.py
@@ -15,15 +15,28 @@ pattern_paper = re.compile(PAPER_REGEXP)
 pattern_poster = re.compile(POSTER_REGEXP)
 pattern_other = re.compile(OTHER_REGEXP)
 
+
+dates = []
+schedule = {}
+sessions = {}
+session_times = {}
+
+
 def parse_order_file(orderfile):
     with open(orderfile, 'r') as in_:
         for line in in_:
             if line == '\n':
                 continue
             if re.match(pattern_day, line):
-                print('DAY', line)
+                cleaned = line.replace('#', '').strip()
+                day, date, year = cleaned.split(',')
+                if (day, date, year) not in dates:
+                    dates.append((day, date, year))
             elif re.match(pattern_session, line):
-                print('SESSION', line)
+                cleaned = line.replace('=Session ', '')
+                code, name = cleaned.split(':')
+                session_number = int(code[:-1])
+                exit()
             elif re.match(pattern_paper, line):
                 print('PAPER', line)
             elif re.match(pattern_poster, line):
@@ -33,6 +46,8 @@ def parse_order_file(orderfile):
             else:
                 print('NOT FOUND')
                 print(line)
+            # exit()
+
 
 if __name__ == "__main__":
     conf = sys.argv[1]
