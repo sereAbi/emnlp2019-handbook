18d096d19088776ae62f8bc3574b1676c62b3f49
marcomilanesio <milanesio.marco@gmail.com>
2019-09-28 14:00:36 +0200

    mm

--- scripts/build_schedule.py
+++ scripts/build_schedule.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import re
+import utils
+from collections import defaultdict
 
 DAY_REGEXP = r"^#\ (Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday)\,\ (November)\ \d{1,2}\, \d{4}"
 SESSION_REGEXP = r"^=Session"
@@ -17,9 +19,14 @@ pattern_other = re.compile(OTHER_REGEXP)
 
 
 dates = []
-schedule = {}
+schedule = defaultdict(defaultdict)
 sessions = {}
 session_times = {}
+
+
+def get_time_range(str_):
+    r = re.compile(r"\d{2}\:\d{2}--\d{2}\:\d{2}\ ")
+    return re.search(r, str_).group().strip()
 
 
 def parse_order_file(orderfile):
@@ -32,17 +39,24 @@ def parse_order_file(orderfile):
                 day, date, year = cleaned.split(',')
                 if (day, date, year) not in dates:
                     dates.append((day, date, year))
+                schedule[day] = {'date': ','.join([day, date, year])}
             elif re.match(pattern_session, line):
+                print(schedule)
                 cleaned = line.replace('=Session ', '')
                 code, name = cleaned.split(':')
                 session_number = int(code[:-1])
+                if re.search('poster', name.lower()):
+                    is_poster = True
                 exit()
             elif re.match(pattern_paper, line):
                 print('PAPER', line)
             elif re.match(pattern_poster, line):
                 print('POSTER', line)
             elif re.match(pattern_other, line):
-                print('OTHER', line)
+                cleaned = line.replace('+ ', '')
+                time_range = get_time_range(cleaned)
+                title = cleaned.split(time_range)[1].strip()
+                schedule[day].update({time_range: title})
             else:
                 print('NOT FOUND')
                 print(line)
