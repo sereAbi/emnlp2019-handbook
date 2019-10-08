b64f53f73143e571e26138b7ff866ea7c8233863
marcomilanesio <milanesio.marco@gmail.com>
2019-10-08 22:41:29 +0200

    new

--- scripts/fix_bibentry.py
+++ scripts/fix_bibentry.py
@@ -3,15 +3,13 @@ import sys
 
 author_ptn = re.compile(r"^AUTHOR")
 sortname_ptn = re.compile(r"^SORTNAME")
-point_ptn = re.compile(r"\. ")
+# point_ptn = re.compile(r"\. ")
 
 fname = sys.argv[1]
 
 with open('tempfile.txt', 'w') as outfile:
     for line in open(fname, 'r').readlines():
-        if re.match(author_ptn, line) and re.search(point_ptn, line):
-            line = line.replace(", ", " and ")
-        if re.match(sortname_ptn, line) and re.search(point_ptn, line):
+        if re.match(author_ptn, line) or re.match(sortname_ptn, line):
             line = line.replace(", ", " and ")
 
         outfile.write(line)
