1b03e7fb370b2ecd5849c8d52c4198027bb37274
marcomilanesio <milanesio.marco@gmail.com>
2019-10-08 22:30:27 +0200

    script to modify the bib file

---
+++ scripts/fix_bibentry.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+author_ptn = re.compile(r"^AUTHOR")
+sortname_ptn = re.compile(r"^SORTNAME")
+point_ptn = re.compile(r"\. ")
+
+fname = sys.argv[1]
+
+with open('tempfile.txt', 'w') as outfile:
+    for line in open(fname, 'r').readlines():
+        if re.match(author_ptn, line) and re.search(point_ptn, line):
+            line = line.replace(", ", " and ")
+        if re.match(sortname_ptn, line) and re.search(point_ptn, line):
+            line = line.replace(", ", " and ")
+
+        outfile.write(line)
+
