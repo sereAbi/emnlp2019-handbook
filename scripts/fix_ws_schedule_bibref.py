e36bafa1b200c7398df0ed179b9b65f5a226dc60
marcomilanesio <milanesio.marco@gmail.com>
2019-10-13 16:55:54 +0200

    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

---
+++ scripts/fix_ws_schedule_bibref.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+for root, subd, files in os.walk('auto'):
+    for f in files:
+        if f == 'schedule.tex':
+            tmp = 'temp.tex'
+            fname = os.path.join(root, f)
+            with open(tmp, 'w') as out:
+                with open(fname, 'r') as in_:
+                    for line in in_.readlines():
+                        if re.search('wspaperentry', line):
+                            name = line.split('wspaperentry')[1]
+                            ws, id_ = name.replace('{', '').replace('}', '').split('-')
+                            newname = '{{ {}-{} }}'.format(ws, int(id_))
+                            out.write(line.replace(name, newname) + '\n')
+                        else:
+                            out.write(line)
+            os.rename(tmp, fname)
+            print(fname, 'done')
